8dacf7154e36a8b684678c0ae061677e8c2296cd
jp-d-p <jpieper@deloitte.de>
2024-05-10 14:35:15 +0200

    modified EDA script

--- scripts/eda_car_data.py
+++ scripts/eda_car_data.py
@@ -1,5 +1,6 @@
-from snowflake.snowpark.functions import col
+from snowflake.snowpark.DataFrame import DataFrame 
+from snowflake.snowpark.functions import col       
 
 def filter_by_country(session, table_name, country):
-  df = session.table(table_name)
-  return df.filter(col("country") == country)
+    df = session.sql("SELECT * FROM {}".format(table_name))
+    return df.filter(col("country") == country)
